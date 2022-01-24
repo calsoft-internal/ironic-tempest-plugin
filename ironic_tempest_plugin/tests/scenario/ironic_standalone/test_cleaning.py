0d7f3eadf15719005f6c0210b0d96ae24f98e1b0
Ameya Raut <rautameya04@gmail.com>
2022-01-24 20:21:30 +0000

    Adding Dell copyright

--- ironic_tempest_plugin/tests/scenario/ironic_standalone/test_cleaning.py
+++ ironic_tempest_plugin/tests/scenario/ironic_standalone/test_cleaning.py
@@ -1,6 +1,8 @@
 #
 # Copyright 2017 Mirantis Inc.
 #
+# Copyright (c) 2021-2022 Dell Inc. or its subsidiaries.
+#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
